b85860c7af275a0da5d35e59956c6fc2e5a7a3a1
Volodymyr Kovalchuk <9an3dw0w@gmail.com>
2019-01-19 20:47:50 +0100

    Add update recipe.

--- src/main/java/com/example/guru/recipeproject/services/RecipeServiceImpl.java
+++ src/main/java/com/example/guru/recipeproject/services/RecipeServiceImpl.java
@@ -48,6 +48,12 @@ public class RecipeServiceImpl implements RecipeService {
         return recipeOptional.get();
     }
 
+    @Override
+    @Transactional
+    public RecipeCommand findCommandById(Long l) {
+        return recipeToRecipeCommand.convert(findById(l));
+    }
+
     @Transactional
     @Override
     public RecipeCommand saveRecipeCommand(RecipeCommand command) {
